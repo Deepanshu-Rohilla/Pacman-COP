5b7efc26daf470e5ee5e54fc6cc7eb60682bc5e2
Deepanshu Rohilla <deepanshu38841@gmail.com>
2021-05-18 02:28:14 +0530

    ghosts added

---
+++ lib/ghosts.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+
+class Ghost1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(2),
+      child: Image.asset('lib/images/ghost.png'),
+    );
+  }
+}
+
+
+class MyGhost2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(2),
+      child: Image.asset('lib/images/ghost2.png'),
+    );
+  }
+}
+
+class MyGhost3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(2),
+      child: Image.asset('lib/images/ghost3.png'),
+    );
+  }
+}
