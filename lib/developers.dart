edc26d2a43f0e6a6c1f2b8a51ad3482d09253d2b
CoderPanda1729 <78403738+CoderPanda1729@users.noreply.github.com>
2021-05-18 20:42:04 +0530

    added developer info

---
+++ lib/developers.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Developers extends StatefulWidget {
+  @override
+  _DevelopersState createState() => _DevelopersState();
+}
+
+class _DevelopersState extends State<Developers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About the developers'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100, bottom: 100, left: 50, right: 50),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('This app is developed by two students of IIT Delhi as a project of the COP290 course'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Deepanshu Rohilla'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Prashant Mishra'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('https://github.com/Deepanshu-Rohilla/Pacman-COP'),
+              )
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+}
